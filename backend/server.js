261c7889a7c35dfb90abfbc5b0112e5faaaed578
Caio Fernandes <caiorlfsouza@gmail.com>
2026-05-05 21:43:45 -0300

    feat: prepara projeto para deploy

--- backend/server.js
+++ backend/server.js
@@ -152,6 +152,8 @@ app.delete("/tarefas/:id", autenticar, async (req, res) => {
     }
 });
 
-app.listen(3001, () => {
-  console.log("Servidor rodando na porta 3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
